4933c12171285a6b7cd8a059495622240489e747
Christian Schieberle <xcs@ier.uni-stuttgart.de>
2018-05-28 15:45:19 +0200

    Adjusted object validity. Sex should be 'M' or 'F' only.

--- R/individual.R
+++ R/individual.R
@@ -9,18 +9,18 @@ Individual <- setClass(
   slots = c(
     id = "character",
     age = "numeric",
-    sex = "numeric",
+    sex = "character",
     edulevel = "numeric"
   ),
   
   prototype = list(
     id = NULL, 
     age = 0, 
-    sex = 0, 
+    sex = "F", 
     edulevel = 0
   ),
   
   validity = function(object) {
-    return(TRUE)
+    return(object@sex %in% c("F", "M") & object@age >= 0)
   }
 )
